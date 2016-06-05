e6be36ea244a8e98a4131a013e914555fe24bbad
thaus <tomislav.haus@fer.hr>
2016-06-05 17:28:13 +0200

    Added config file with sensitivity gaine. Modified main loop by publishing an additional topic with scaled force-torque values.. Added launch filer where the gains are loaded into rosparam server.

--- src/optoforce_sensor_node.cpp
+++ src/optoforce_sensor_node.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "optoforce_sensor/opto.h"
 #include "ros/ros.h"
-#include "geometry_msgs/Wrench.h"
+#include "geometry_msgs/WrenchStamped.h"
 #include <unistd.h>
+#include <string.h>
 
 void msSleep(unsigned long p_uMillisecs)
 {
@@ -17,7 +18,13 @@ int main(int argc, char **argv)
     OptoPorts ports;
     msSleep(2500); // We wait some ms to be sure about OptoPorts enumerated PortList
 
+    double fx_gain, fy_gain, fz_gain;  // sensitivity gain in counts/N
+    double tx_gain, ty_gain, tz_gain;  // sensitivity gain in count/Nm
+
+    u_int8_t sens_params_set = 1;
+
     OPort* portlist=ports.listPorts(true);
+
 
     if (ports.getLastSize()>0)
     {
@@ -39,27 +46,63 @@ int main(int argc, char **argv)
             //init ROS node
             std::cout<<"Found 6DOF sensor -> Starting ROS node"<<std::endl;
             ros::init(argc, argv, "OForceSensorPublisher");
-            ros::NodeHandle n;
+            ros::NodeHandle n("~");
             //Creating wrench publisher
-            ros::Publisher wrench_pub = n.advertise<geometry_msgs::Wrench>("OptoForceWrench", 1000);
-            //Set ROS rate to 10 Hz
+            ros::Publisher wrench_pub_raw = n.advertise<geometry_msgs::WrenchStamped>("OptoForceWrench_raw", 1000);
+            ros::Publisher wrench_pub = n.advertise<geometry_msgs::WrenchStamped>("OptoForceWrench", 1000);
+
+            // get sensitivy gains from rosparam server
+            n.param("fx_gain", fx_gain, 0.0);
+            n.param("fy_gain", fy_gain, 0.0);
+            n.param("fz_gain", fz_gain, 0.0);
+
+            n.param("tx_gain", tx_gain, 0.0);
+            n.param("ty_gain", ty_gain, 0.0);
+            n.param("tz_gain", tz_gain, 0.0);
+
+            double double_thresh = 0.0001;
+
+            if (fx_gain < 0.0001 || fy_gain < 0.0001 || fz_gain < 0.001) {
+                std::cout<<"Sensitivity force params not set properly. Publishing only raw values."<<std::endl;
+                sens_params_set = 0;
+            }
+
+            if (tx_gain < 0.0001 || ty_gain < 0.0001 || tz_gain < 0.001) {
+                std::cout<<"Sensitivity torque params not set properly.Publishing only raw values."<<std::endl;
+                sens_params_set = 0;
+            }
+
+            //Set ROS rate to 100 Hz
             ros::Rate loop_rate(100);
             //Main ROS loop
             while(ros::ok())
             {
-                geometry_msgs::Wrench wrench_msg; // Create msg
+                geometry_msgs::WrenchStamped wrench_msg; // Create msg
                 //Fill msg
-                wrench_msg.force.x = pack6D.Fx; 
-                wrench_msg.force.y = pack6D.Fy;
-                wrench_msg.force.z = pack6D.Fz;
-                wrench_msg.torque.x = pack6D.Tx;
-                wrench_msg.torque.y = pack6D.Ty;
-                wrench_msg.torque.z = pack6D.Tz;
-                wrench_pub.publish(wrench_msg);
+                wrench_msg.header.stamp = ros::Time::now();
+                wrench_msg.wrench.force.x = pack6D.Fx;
+                wrench_msg.wrench.force.y = pack6D.Fy;
+                wrench_msg.wrench.force.z = pack6D.Fz;
+                wrench_msg.wrench.torque.x = pack6D.Tx;
+                wrench_msg.wrench.torque.y = pack6D.Ty;
+                wrench_msg.wrench.torque.z = pack6D.Tz;
+                wrench_pub_raw.publish(wrench_msg);
+
+                if (sens_params_set) {
+                    wrench_msg.header.stamp = ros::Time::now();
+                    wrench_msg.wrench.force.x = pack6D.Fx / fx_gain;
+                    wrench_msg.wrench.force.y = pack6D.Fy / fy_gain;
+                    wrench_msg.wrench.force.z = pack6D.Fz / fz_gain;
+                    wrench_msg.wrench.torque.x = pack6D.Tx / tx_gain;
+                    wrench_msg.wrench.torque.y = pack6D.Ty / ty_gain;
+                    wrench_msg.wrench.torque.z = pack6D.Tz / tz_gain;
+                    wrench_pub.publish(wrench_msg);
+                }
+
                 ros::spinOnce();
                 loop_rate.sleep();
                 size = daq.read6D(pack6D,false);
-		std::cout<<size<<std::endl;
+                std::cout<<size<<std::endl;
             }
             //std::cout<<"Fx: "<<pack6D.Fx<<" Fy: "<<pack6D.Fy<<" Fz: "<<pack6D.Fz<<" ";
             //std::cout<<"Tx: "<<pack6D.Tx<<" Ty: "<<pack6D.Ty<<" Tz: "<<pack6D.Tz<<std::endl;
